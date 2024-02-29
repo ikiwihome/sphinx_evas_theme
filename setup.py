2ae16c7b2153c52534c450c987ada8e3ac837de3
ikiwi <ikiwicc@gmail.com>
2024-03-01 02:38:38 +0800

    update version

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class TransifexCommand(distutils.cmd.Command):
 
 
 setup(
-    version='0.1.0',
+    version='0.2.0',
     cmdclass={
         'update_translations': UpdateTranslationsCommand,
         'transifex': TransifexCommand,
