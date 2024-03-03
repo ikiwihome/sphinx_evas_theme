23dbd85fbf8a57b8cf48ec149e40abc82fe9a197
ikiwi <ikiwicc@gmail.com>
2024-03-03 17:38:59 +0800

    add some extensions

--- sphinx_evas_theme/__init__.py
+++ sphinx_evas_theme/__init__.py
@@ -152,7 +152,6 @@ def setup(app: "Sphinx"):
     app.setup_extension('sphinx.ext.viewcode')
     app.setup_extension('sphinxcontrib.mermaid')
     app.setup_extension('sphinxcontrib.getthecode')
-    app.setup_extension('sphinxcontrib.drawio')
 
     app.add_source_suffix('.rst', 'restructuredtext', True)
     app.add_source_suffix('.md', 'markdown', True)
