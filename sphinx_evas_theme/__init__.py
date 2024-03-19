487dff1c56cb62a5126fd74a74fd758d7cf7d03c
ikiwicc <ikiwicc@gmail.com>
2024-03-19 13:27:20 +0800

    update version and latex style

--- sphinx_evas_theme/__init__.py
+++ sphinx_evas_theme/__init__.py
@@ -16,7 +16,7 @@ from sphinx.locale import _
 from sphinx.util.logging import getLogger
 
 
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 __version_full__ = __version__
 
 logger = getLogger(__name__)
